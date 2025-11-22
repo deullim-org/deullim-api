7ef562d989fc88f9e550536c838fe3e27e61b383
byungwook-min <byungwook-min@naver.com>
2025-11-23 02:14:44 +0900

    feat: initial setup deullim-api project

---
+++ README.md
@@ -0,0 +1,1 @@
+# deullim-api
---
+++ src/test/kotlin/org/deullim/api/DeullimApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.deullim.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeullimApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
