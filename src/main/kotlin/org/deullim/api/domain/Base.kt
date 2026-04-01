cd09a5ff23c5b361aae1333f223e78957a874b83
Subin Min <tnqls5417@naver.com>
2026-04-01 14:47:41 +0900

    feat: add Base Entity

---
+++ src/main/kotlin/org/deullim/api/domain/Base.kt
@@ -0,0 +1,24 @@
+package org.deullim.api.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.Version
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class Base(
+    @Version
+    var version: Long = 0,
+
+    @CreatedDate
+    @Column(updatable = false)
+    var createdAt: Instant? = null,
+
+    @LastModifiedDate
+    var updatedAt: Instant? = null,
+)
