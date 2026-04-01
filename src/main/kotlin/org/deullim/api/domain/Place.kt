58bef15bd7fcf72f2de081d0163f71ed633c9dee
Subin Min <tnqls5417@naver.com>
2026-04-01 14:48:06 +0900

    feat: Place 내 Jpa mapping 추가

--- src/main/kotlin/org/deullim/api/domain/Place.kt
+++ src/main/kotlin/org/deullim/api/domain/Place.kt
@@ -2,9 +2,12 @@ package org.deullim.api.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
@@ -24,28 +27,25 @@ class Place protected constructor() : Base() {
     var icon: String? = null
         protected set
 
-    // TODO: 연관 처리
-    @Column(name = "location_id", nullable = false, updatable = false)
-    var locationId: Long = 0
-        protected set
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "location_id", nullable = false, updatable = false)
+//    lateinit var location: Location
+//        protected set
 
-    // TODO: 연관 처리
-    @Column(name = "member_id", nullable = false, updatable = false)
-    var memberId: Long = 0
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false, updatable = false)
+    lateinit var member: Member
         protected set
 
     companion object {
         fun create(
-            memberId: Long,
-            locationId: Long,
+            member: Member,
+//            location: Location,
             alias: String? = null,
             icon: String? = null,
         ): Place = Place().apply {
-            require(memberId > 0) { "memberId must be positive" }
-            require(locationId > 0) { "locationId must be positive" }
-
-            this.memberId = memberId
-            this.locationId = locationId
+            this.member = member
+//            this.location = location
             this.alias = alias
             this.icon = icon
         }
