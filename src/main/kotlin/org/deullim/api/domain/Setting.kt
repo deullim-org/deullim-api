e7d129e10f66b0b142686ea6f92e41b26c157c9c
Subin Min <tnqls5417@naver.com>
2026-04-01 14:48:11 +0900

    feat: Setting 내 Jpa mapping 추가

--- src/main/kotlin/org/deullim/api/domain/Setting.kt
+++ src/main/kotlin/org/deullim/api/domain/Setting.kt
@@ -2,13 +2,21 @@ package org.deullim.api.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 @Entity
-@Table(name = "settings")
+@Table(name = "settings",
+    uniqueConstraints = [
+        UniqueConstraint(name = "uk_settings_member_id", columnNames = ["member_id"])
+    ]
+)
 class Setting protected constructor() : Base() {
 
     @Id
@@ -20,9 +28,9 @@ class Setting protected constructor() : Base() {
     var radius: Double = DEFAULT_RADIUS_METER
         protected set
 
-    // TODO: 연관처리
-    @Column(name = "member_id", nullable = false, updatable = false)
-    var memberId: Long = 0
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false, updatable = false)
+    lateinit var member: Member
         protected set
 
     companion object {
@@ -30,15 +38,14 @@ class Setting protected constructor() : Base() {
         const val MAX_RADIUS_METER: Double = 2000.0
         const val DEFAULT_RADIUS_METER: Double = 200.0
 
-        fun create(memberId: Long, radius: Double?): Setting {
-            require(memberId > 0) { "memberId must be positive" }
+        fun create(member: Member, radius: Double?): Setting {
             if (radius != null) {
                 require(radius in MIN_RADIUS_METER..MAX_RADIUS_METER) {
                     "radius must be between $MIN_RADIUS_METER and $MAX_RADIUS_METER meters"
                 }
             }
             return Setting().apply {
-                this.memberId = memberId
+                this.member = member
                 this.radius = radius ?: DEFAULT_RADIUS_METER
             }
         }
